9e24613fb09cba87394c8339037431cf86a16433
Markus Peltola <41840909+markus-peltola@users.noreply.github.com>
2023-09-29 14:41:23 +0300

    Added new functions to purchases
    purchaseInvoiceList(), getPurchaseInvoice() and purchaseOrder()

--- src/interfaces/purchases.ts
+++ src/interfaces/purchases.ts
@@ -215,3 +215,328 @@ export interface PurchaseInvoiceSubLine {
   description?: string;
   sort?: number;
 }
+
+/*
+ * RESOURCE
+ * purchaseinvoicelist.nv
+ */
+
+export interface PurchaseInvoiceListParameters {
+  beginInvoiceDate?: string;
+  endInvoiceDate?: string;
+  invoiceNumber?: string;
+  invoiceStatus?: 'open' | 'approved' | 'accepted';
+  /** yyyy-MM-ddTHH:mm:ss */
+  lastModifiedStart?: string;
+  /** yyyy-MM-ddTHH:mm:ss */
+  lastModifiedEnd?: string;
+  purchaseInvoiceBatchId?: number;
+  paymentStatus?: 'unpaid' | 'paid';
+}
+
+export interface PurchaseInvoiceListItem {
+  netvisorKey: number;
+  invoiceNumber: string;
+  invoiceDate: {
+    value: string;
+    attr: { format: 'ansi' };
+  };
+  vendor: string;
+  vendorOrganizationIdentifier: string;
+  sum: number;
+  payments: number;
+  openSum: number;
+  uri: string;
+}
+
+/*
+ * RESOURCE
+ * getpurchaseinvoice.nv
+ */
+
+export interface GetPurchaseInvoiceParameters {
+  netvisorKey?: number;
+  /** Max 500 ids */
+  netvisorKeyList?: string;
+  include?: 'actions' | 'handlinghistory' | 'previewimage' | 'invoiceimage';
+  omitAttachments?: boolean;
+}
+
+export interface GetPurchaseInvoice {
+  purchaseInvoiceNetvisorKey: number;
+  purchaseInvoiceNumber: string;
+  purchaseInvoiceDate: {
+    value: string;
+    attr: { format: 'ansi' };
+  };
+  purchaseInvoiceEventDate?: {
+    value: string;
+    attr: { format: 'ansi' };
+  };
+  purchaseInvoiceDeliveryDate?: {
+    value: string;
+    attr: { format: 'ansi' };
+  };
+  purchaseInvoiceDueDate: {
+    value: string;
+    attr: { format: 'ansi' };
+  };
+  purchaseInvoiceValueDate?: {
+    value: string;
+    attr: { format: 'ansi' };
+  };
+  purchaseInvoiceReferenceNumber?: string;
+  purchaseInvoiceAgreementIdentifier?: string;
+  purchaseInvoiceVendorBankAccountNumber?: string;
+  isPurchaseInvoiceVendorBankAccountDeleted?: boolean;
+  isPurchaseInvoiceVendorBankAccountFromSEPARegion?: boolean;
+  purchaseInvoiceAmount: number;
+  purchaseInvoicePaidAmount: number;
+  foreignCurrencyAmount?: number;
+  foreignCurrencyNameID?: string;
+  invoiceStatus: string;
+  approvalStatus: string;
+  purchaseInvoiceOurReference?: string;
+  purchaseInvoiceYourReference?: string;
+  purchaseInvoiceDescription?: string;
+  vendorNetvisorKey: number;
+  vendorOrganizationIdentifier?: string;
+  vendorCode?: string;
+  vendorName: string;
+  vendorAddressLine?: string;
+  vendorPostNumber?: string;
+  vendorTown?: string;
+  vendorCountry?: string;
+  fingerprint: string;
+  voucherId?: number;
+  isAccounted: boolean;
+  previewImage?: {
+    value: string;
+    attr: { attachmentNetvisorKey: number };
+  };
+  invoiceImage?: {
+    value: string;
+    attr: { attachmentNetvisorKey: number };
+  };
+  attachments?: {
+    attachment: {
+      attachmentBase64Data: string;
+      fileName: string;
+      contentType: string;
+      comment?: string;
+    }[];
+  };
+  invoiceLines: {
+    purchaseInvoiceLine: GetPurchaseInvoiceLine[];
+  };
+  linkedPurchaseOrders?: {
+    purchaseOrder: {
+      orderNumber: string;
+      netvisorKey?: number;
+    }[];
+  };
+  postingLinesAccess?: {
+    canEditPostingLines: boolean;
+    canUserPostInvoice: boolean;
+    suggestPostingByDefault: boolean;
+    canEditAccountingSuggestion: boolean;
+  };
+  actions?: {
+    action: {
+      id: string;
+      type: string;
+      description: string;
+    }[];
+  };
+  alerts?: {
+    alert: {
+      heading: string;
+      description: string;
+    }[];
+  };
+  notifications?: {
+    notification: {
+      heading: string;
+      description: string;
+    }[];
+  };
+  handlingHistory?: {
+    handlingHistoryLine: GetPurchaseInvoiceHandlingHistoryLine[];
+  };
+}
+
+export interface GetPurchaseInvoiceLine {
+  netvisorKey: number;
+  lineSum: number;
+  lineNetSum: number;
+  unitPrice: number;
+  vatPercent: number;
+  vatCode: string;
+  description?: string;
+  unit?: string;
+  orderedAmount: number;
+  deliveredAmount: number;
+  productCode?: string;
+  discountPercentage: number;
+  productName?: string;
+  accountingSuggestionBookkeepingAccount?: string;
+  accountingSuggestionBookkeepingAccountNetvisorKey?: number;
+  purchaseInvoiceLineDimensions?: {
+    dimension: {
+      dimensionName: string;
+      dimensionNameNetvisorKey: number;
+      dimensionDetailName: string;
+      dimensionDetailNameNetvisorKey: number;
+    }[];
+  };
+}
+
+export interface GetPurchaseInvoiceHandlingHistoryLine {
+  type: string;
+  heading: string;
+  description: string;
+  timestamp: {
+    value: string;
+    attr: { format: 'ansi' };
+  };
+  userName: string;
+  userEmail?: string;
+  updatedInformationFields?: {
+    fieldName: string;
+    oldValue: string;
+    newValue: string;
+  }[];
+}
+
+/*
+ * RESOURCE
+ * purchaseorder.nv
+ */
+
+export interface PurchaseOrderParameters {
+  method: 'add' | 'edit';
+  /** Must be given if method is 'edit' */
+  id?: number;
+}
+
+export interface PurchaseOrder {
+  orderNumber?: string;
+  orderStatus?: 'proposal' | 'approved' | 'senttovendor' | 'archived';
+  orderDate?: {
+    value: string;
+    attr: { format: 'ansi' };
+  };
+  vendorIdentifier?: {
+    value: string;
+    attr: { type: 'netvisor' | 'code' | 'organisationidentifier' };
+  };
+  purchaseOrderVendorDetails?: {
+    address?: string;
+    postNumber?: string;
+    city?: string;
+    country?: {
+      value: string;
+      attr: { type: 'ISO-3166' };
+    };
+  };
+  deliveryTerm?: string;
+  deliveryMethod?: string;
+  purchaseOrderDeliveryDetails?: {
+    name?: string;
+    address?: string;
+    postNumber?: string;
+    deliveryCity?: string;
+    deliveryCountry?: {
+      value: string;
+      attr: { type: 'ISO-3166' };
+    };
+  };
+  privateComment?: string;
+  comment?: string;
+  ourReference?: string;
+  currencyCode?: {
+    value: string;
+    attr: { type: 'ISO-4217' };
+  };
+  purchaseOrderPaymentTerm?: {
+    netDays?: number;
+    discountDays?: number;
+    discountPercent?: number;
+  };
+  purchaseOrderLines?: {
+    purchaseOrderProductLine?: PurchaseOrderProductLine[];
+    purchaseOrderCommentLine?: {
+      attr?: { method?: 'add' | 'edit' | 'delete'; netvisorKey?: number };
+      comment?: string;
+    }[];
+  };
+}
+
+export interface PurchaseOrderProductLine {
+  attr?: {
+    method?: 'add' | 'edit' | 'delete' | 'partialdeliver' | 'addtopartialdeliverygroup';
+    netvisorKey?: number;
+  };
+  purchaseOrderProductLinePartialDelivery?: {
+    purchaseOrderProductLinePartialDeliveryContent?: {
+      orderedAmount?: number;
+      deliveredAmount?: number;
+      deliveryReceivedDate?: {
+        value: string;
+        attr: { format: 'ansi' };
+      };
+      deliveryManuallyConfirmed?: boolean;
+      deliveredProductQuality?: number;
+      inventoryBatchNumber?: number;
+      inventoryManufactureDate?: {
+        value: string;
+        attr: { format: 'ansi' };
+      };
+      inventoryPackagingDate?: {
+        value: string;
+        attr: { format: 'ansi' };
+      };
+      inventoryBestBeforeDate?: {
+        value: string;
+        attr: { format: 'ansi' };
+      };
+      inventoryExpiryDate?: {
+        value: string;
+        attr: { format: 'ansi' };
+      };
+    }[];
+  };
+  productCode?: {
+    value: string;
+    attr: { type: 'netvisor' | 'customer' };
+  };
+  vendorProductCode?: string;
+  orderedAmount?: number;
+  unitPrice?: number;
+  vatPercent?: number;
+  freightRate?: number;
+  lineComment?: string;
+  deliveryDate?: {
+    value: string;
+    attr: { format: 'ansi' };
+  };
+  purchaseOrderProductLineDeliveryDetails?: {
+    deliveredAmount?: number;
+    deliveryReceivedDate?: {
+      value: string;
+      attr: { format: 'ansi' };
+    };
+    deliveryManuallyConfirmed?: boolean;
+    deliveredProductQuality?: number;
+  };
+  inventoryPlace?: {
+    value: string;
+    attr: { type: 'netvisor' | 'customer' };
+  };
+  purchaseOrderProductLineDimensions?: {
+    dimension: {
+      dimensionName: string;
+      dimensionItem: string;
+    }[];
+  };
+}
--- src/methods/purchases.ts
+++ src/methods/purchases.ts
@@ -1,5 +1,17 @@
 import { NetvisorApiClient } from '..';
-import { GetVendorParameters, GetVendor, PurchaseInvoice } from '../interfaces/purchases';
+import {
+  GetVendorParameters,
+  GetVendor,
+  PurchaseInvoice,
+  PurchaseInvoiceListParameters,
+  PurchaseInvoiceListItem,
+  GetPurchaseInvoiceParameters,
+  GetPurchaseInvoice,
+  GetPurchaseInvoiceHandlingHistoryLine,
+  GetPurchaseInvoiceLine,
+  PurchaseOrder,
+  PurchaseOrderParameters
+} from '../interfaces/purchases';
 import { NetvisorMethod, parseXml, buildXml, forceArray } from './_method';
 
 export class NetvisorPurchasesMethod extends NetvisorMethod {
@@ -154,5 +166,229 @@ export class NetvisorPurchasesMethod extends NetvisorMethod {
   async purchaseInvoice(purchaseInvoice: PurchaseInvoice): Promise<string> {
     const response = await this._client.post('purchaseinvoice.nv', buildXml({ root: { purchaseInvoice: purchaseInvoice } }));
     return parseXml(response).replies.inserteddataidentifier;
+  }
+
+  /**
+   * Get purchase invoice list from Netvisor
+   * @example await purchaseInvoiceList({ beginInvoiceDate: '2023-09-01', endInvoiceDate: '2023-09-30', invoiceStatus: 'Open' });
+   */
+  async purchaseInvoiceList(params?: PurchaseInvoiceListParameters): Promise<PurchaseInvoiceListItem[]> {
+    // Get the raw xml response from Netvisor
+    const responseXml = await this._client.get('purchaseinvoicelist.nv', params);
+    // Parse the xml to js object
+    const xmlObject: any = parseXml(responseXml);
+    // Create the return array
+    const purchaseInvoiceList: PurchaseInvoiceListItem[] = [];
+    // Add items to return array
+    if (xmlObject.purchaseinvoicelist?.purchaseinvoice) {
+      forceArray(xmlObject.purchaseinvoicelist.purchaseinvoice).forEach((xmlInvoice: any) => {
+        purchaseInvoiceList.push({
+          netvisorKey: parseInt(xmlInvoice.netvisorkey),
+          invoiceNumber: xmlInvoice.invoicenumber,
+          invoiceDate: xmlInvoice.invoicedate,
+          vendor: xmlInvoice.vendor,
+          vendorOrganizationIdentifier: xmlInvoice.vendororganizationidentifier,
+          sum: parseFloat(xmlInvoice.sum.replace(',', '.')),
+          payments: parseFloat(xmlInvoice.payments.replace(',', '.')),
+          openSum: parseFloat(xmlInvoice.opensum.replace(',', '.')),
+          uri: xmlInvoice.uri
+        });
+      });
+    }
+
+    return purchaseInvoiceList;
+  }
+
+  /**
+   * Get purchase invoice(s) from Netvisor
+   * @example await getPurchaseInvoice({ netvisorKey: 123 });
+   */
+  async getPurchaseInvoice(params: GetPurchaseInvoiceParameters): Promise<GetPurchaseInvoice[]> {
+    // Get the raw xml response from Netvisor
+    const responseXml = await this._client.get('getpurchaseinvoice.nv', { ...params, version: '2' });
+    // Parse the xml to js object
+    const xmlObject: any = parseXml(responseXml);
+    // Create the return array
+    const purchaseInvoices: GetPurchaseInvoice[] = [];
+    // Parse invoices from xml object
+    let xmlInvoices = [];
+    if (xmlObject.purchaseinvoice) {
+      xmlInvoices = forceArray(xmlObject.purchaseinvoice);
+    } else if (xmlObject.purchaseinvoices?.purchaseinvoice) {
+      xmlInvoices = forceArray(xmlObject.purchaseinvoices.purchaseinvoice);
+    }
+    // Add items to return array
+    xmlInvoices.forEach((xmlInvoice: any) => {
+      // Create the return object with required attributes
+      const purchaseInvoice: GetPurchaseInvoice = {
+        purchaseInvoiceNetvisorKey: parseInt(xmlInvoice.purchaseinvoicenetvisorkey),
+        purchaseInvoiceNumber: xmlInvoice.purchaseinvoicenumber,
+        purchaseInvoiceDate: xmlInvoice.purchaseinvoicedate,
+        purchaseInvoiceDueDate: xmlInvoice.purchaseinvoiceduedate,
+        purchaseInvoiceReferenceNumber: xmlInvoice.purchaseinvoicereferencenumber || undefined,
+        purchaseInvoiceAgreementIdentifier: xmlInvoice.purchaseinvoiceagreementidentifier || undefined,
+        purchaseInvoiceAmount: parseFloat(xmlInvoice.purchaseinvoiceamount.replace(',', '.')),
+        purchaseInvoicePaidAmount: parseFloat(xmlInvoice.purchaseinvoicepaidamount.replace(',', '.')),
+        invoiceStatus: xmlInvoice.invoicestatus,
+        approvalStatus: xmlInvoice.approvalstatus,
+        purchaseInvoiceOurReference: xmlInvoice.purchaseinvoiceourreference || undefined,
+        purchaseInvoiceYourReference: xmlInvoice.purchaseinvoiceyourreference || undefined,
+        purchaseInvoiceDescription: xmlInvoice.purchaseinvoicedescription || undefined,
+        vendorNetvisorKey: parseInt(xmlInvoice.vendornetvisorkey),
+        vendorOrganizationIdentifier: xmlInvoice.vendororganizationidentifier || undefined,
+        vendorCode: xmlInvoice.vendorcode || undefined,
+        vendorName: xmlInvoice.vendorname,
+        vendorAddressLine: xmlInvoice.vendoraddressline || undefined,
+        vendorPostNumber: xmlInvoice.vendorpostnumber || undefined,
+        vendorTown: xmlInvoice.vendortown || undefined,
+        vendorCountry: xmlInvoice.vendorcountry || undefined,
+        fingerprint: xmlInvoice.fingerprint,
+        isAccounted: xmlInvoice.isaccounted === 'True' ? true : false,
+        invoiceLines: { purchaseInvoiceLine: [] }
+      };
+
+      // Add optional attributes
+      if (xmlInvoice.purchaseinvoiceeventdate?.value) purchaseInvoice.purchaseInvoiceEventDate = xmlInvoice.purchaseinvoiceeventdate;
+      if (xmlInvoice.purchaseinvoicedeliverydate?.value)
+        purchaseInvoice.purchaseInvoiceDeliveryDate = xmlInvoice.purchaseinvoicedeliverydate;
+      if (xmlInvoice.purchaseinvoicevaluedate?.value) purchaseInvoice.purchaseInvoiceValueDate = xmlInvoice.purchaseinvoicevaluedate;
+      if (xmlInvoice.purchaseinvoicevendorbankaccountnumber) {
+        purchaseInvoice.purchaseInvoiceVendorBankAccountNumber = xmlInvoice.purchaseinvoicevendorbankaccountnumber;
+        purchaseInvoice.isPurchaseInvoiceVendorBankAccountDeleted =
+          xmlInvoice.ispurchaseinvoicevendorbankaccountdeleted === 'True' ? true : false;
+        purchaseInvoice.isPurchaseInvoiceVendorBankAccountFromSEPARegion =
+          xmlInvoice.ispurchaseinvoicevendorbankaccountfromseparegion === 'True' ? true : false;
+      }
+      if (xmlInvoice.foreigncurrencyamount) {
+        purchaseInvoice.foreignCurrencyAmount = parseFloat(xmlInvoice.foreigncurrencyamount.replace(',', '.'));
+        purchaseInvoice.foreignCurrencyNameID = xmlInvoice.foreigncurrencynamenameid || undefined;
+      }
+      if (xmlInvoice.voucherid) purchaseInvoice.voucherId = parseInt(xmlInvoice.voucherid);
+      if (xmlInvoice.previewimage) {
+        purchaseInvoice.previewImage = {
+          value: xmlInvoice.previewimage.value,
+          attr: { attachmentNetvisorKey: parseInt(xmlInvoice.previewimage.attr.attachmentnetvisorkey) }
+        };
+      }
+      if (xmlInvoice.invoiceimage) {
+        purchaseInvoice.invoiceImage = {
+          value: xmlInvoice.invoiceimage.value,
+          attr: { attachmentNetvisorKey: parseInt(xmlInvoice.invoiceimage.attr.attachmentnetvisorkey) }
+        };
+      }
+      if (xmlInvoice.attachments?.attachment) {
+        purchaseInvoice.attachments = { attachment: [] };
+        forceArray(xmlInvoice.attachments.attachment).forEach((xmlAttachment: any) => {
+          purchaseInvoice.attachments!.attachment.push({
+            attachmentBase64Data: xmlAttachment.attachmentbase64data,
+            fileName: xmlAttachment.filename,
+            contentType: xmlAttachment.contenttype,
+            comment: xmlAttachment.comment || undefined
+          });
+        });
+      }
+      if (xmlInvoice.linkedpurchaseorders?.purchaseorder) {
+        purchaseInvoice.linkedPurchaseOrders = { purchaseOrder: [] };
+        forceArray(xmlInvoice.linkedpurchaseorders.purchaseorder).forEach((xmlPurchaseOrder: any) => {
+          purchaseInvoice.linkedPurchaseOrders!.purchaseOrder.push({
+            orderNumber: xmlPurchaseOrder.ordernumber,
+            netvisorKey: parseInt(xmlPurchaseOrder.netvisorkey) || undefined
+          });
+        });
+      }
+      if (xmlInvoice.postinglinesaccess) {
+        purchaseInvoice.postingLinesAccess = {
+          canEditPostingLines: xmlInvoice.postinglinesaccess.caneditpostinglines === 'True' ? true : false,
+          canUserPostInvoice: xmlInvoice.postinglinesaccess.canuserpostinvoice === 'True' ? true : false,
+          suggestPostingByDefault: xmlInvoice.postinglinesaccess.suggestpostingbydefault === 'True' ? true : false,
+          canEditAccountingSuggestion: xmlInvoice.postinglinesaccess.caneditaccountingsuggestion === 'True' ? true : false
+        };
+      }
+      if (xmlInvoice.actions?.action) {
+        purchaseInvoice.actions = { action: [] };
+        forceArray(xmlInvoice.actions.action).forEach((xmlAction: any) => {
+          purchaseInvoice.actions!.action.push(xmlAction);
+        });
+      }
+      if (xmlInvoice.alerts?.alert) {
+        purchaseInvoice.alerts = { alert: [] };
+        forceArray(xmlInvoice.alerts.alert).forEach((xmlAlert: any) => {
+          purchaseInvoice.alerts!.alert.push(xmlAlert);
+        });
+      }
+      if (xmlInvoice.notifications?.notification) {
+        purchaseInvoice.notifications = { notification: [] };
+        forceArray(xmlInvoice.notifications.notification).forEach((xmlNotification: any) => {
+          purchaseInvoice.notifications!.notification.push(xmlNotification);
+        });
+      }
+      if (xmlInvoice.handlinghistory?.handlinghistoryline) {
+        purchaseInvoice.handlingHistory = { handlingHistoryLine: [] };
+        forceArray(xmlInvoice.handlinghistory.handlinghistoryline).forEach((xmlHandlingHistoryLine: any) => {
+          const handlingHistoryLine: GetPurchaseInvoiceHandlingHistoryLine = {
+            type: xmlHandlingHistoryLine.type,
+            heading: xmlHandlingHistoryLine.heading,
+            description: xmlHandlingHistoryLine.description,
+            timestamp: xmlHandlingHistoryLine.timestamp,
+            userName: xmlHandlingHistoryLine.username,
+            userEmail: xmlHandlingHistoryLine.useremail || undefined
+          };
+          if (xmlHandlingHistoryLine.updatedinformationfields) {
+            handlingHistoryLine.updatedInformationFields = forceArray(xmlHandlingHistoryLine.updatedinformationfields);
+          }
+          purchaseInvoice.handlingHistory!.handlingHistoryLine.push(handlingHistoryLine);
+        });
+      }
+
+      // Add invoice lines
+      if (xmlInvoice.invoicelines?.purchaseinvoiceline) {
+        forceArray(xmlInvoice.invoicelines.purchaseinvoiceline).forEach((xmlInvoiceLine: any) => {
+          const invoiceLine: GetPurchaseInvoiceLine = {
+            netvisorKey: parseInt(xmlInvoiceLine.netvisorkey),
+            lineSum: parseFloat(xmlInvoiceLine.linesum.replace(',', '.')),
+            lineNetSum: parseFloat(xmlInvoiceLine.linenetsum.replace(',', '.')),
+            unitPrice: parseFloat(xmlInvoiceLine.unitprice.replace(',', '.')),
+            vatPercent: parseFloat(xmlInvoiceLine.vatpercent.replace(',', '.')),
+            vatCode: xmlInvoiceLine.vatcode,
+            description: xmlInvoiceLine.description || undefined,
+            unit: xmlInvoiceLine.unit || undefined,
+            orderedAmount: parseFloat(xmlInvoiceLine.orderedamount.replace(',', '.')) || 0,
+            deliveredAmount: parseFloat(xmlInvoiceLine.deliveredamount.replace(',', '.')) || 0,
+            productCode: xmlInvoiceLine.productcode || undefined,
+            discountPercentage: parseFloat(xmlInvoiceLine.discountpercentage.replace(',', '.')) || 0,
+            productName: xmlInvoiceLine.productname || undefined,
+            accountingSuggestionBookkeepingAccount: xmlInvoiceLine.accountingsuggestionbookkeepingaccount || undefined,
+            accountingSuggestionBookkeepingAccountNetvisorKey:
+              parseInt(xmlInvoiceLine.accountingsuggestionbookkeepingaccountnetvisorkey) || undefined
+          };
+          if (xmlInvoiceLine.purchaseinvoicelinedimensions?.dimension) {
+            invoiceLine.purchaseInvoiceLineDimensions = { dimension: [] };
+            forceArray(xmlInvoiceLine.purchaseinvoicelinedimensions.dimension).forEach((xmlDimension: any) => {
+              invoiceLine.purchaseInvoiceLineDimensions!.dimension.push({
+                dimensionName: xmlDimension.dimensionname,
+                dimensionNameNetvisorKey: parseInt(xmlDimension.dimensionnamenetvisorkey),
+                dimensionDetailName: xmlDimension.dimensiondetailname,
+                dimensionDetailNameNetvisorKey: parseInt(xmlDimension.dimensiondetailnamenetvisorkey)
+              });
+            });
+          }
+          purchaseInvoice.invoiceLines.purchaseInvoiceLine.push(invoiceLine);
+        });
+      }
+      purchaseInvoices.push(purchaseInvoice);
+    });
+
+    return purchaseInvoices;
+  }
+
+  /**
+   * Create or edit a purchase order in Netvisor. If you are creating a new purchase order, check required fields from Netvisor's documentation.
+   * @example await purchaseOrder(purchaseOrder, { method: 'add' });
+   * @returns the added purchase order's netvisor key
+   */
+  async purchaseOrder(purchaseOrder: PurchaseOrder, params: PurchaseOrderParameters): Promise<string> {
+    const response = await this._client.post('purchaseorder.nv', buildXml({ root: { purchaseOrder: purchaseOrder } }), params);
+    if (params.method === 'add') return parseXml(response).replies.inserteddataidentifier;
+    return params.id?.toString() || '';
   }
 }
